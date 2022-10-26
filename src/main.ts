3cd66cc05dc6ad841c701f115bd236da82006be5
Juan José Rubio Iglesias <jjrubio@talentosoftware.com>
2022-10-26 12:43:21 +0200

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        url: 'http://localhost',
+        port: 3001,
+      },
+      // options: {
+      //   //url: process.env.GRPC_URL_RECURSOS,
+      //   package: 'auth',
+      //   // protoPath: join(__dirname, 'Application/grpc/recursos.proto'),
+      // },
+    },
+  );
+  //app.useGlobalPipes(new ValidationPipe());
+  app.listen();
 }
 bootstrap();
