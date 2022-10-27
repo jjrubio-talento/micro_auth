8528b30a3dc0e2b3403e5096c6df21cc2574e282
Juan José Rubio Iglesias <jjrubio@talentosoftware.com>
2022-10-27 14:25:26 +0200

    Test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginRequest, LoginResponse } from './dto/auth.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/hello')
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Post('/login')
+  login(@Body() loginRequest: LoginRequest) {
+    return this.appService.login(loginRequest);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { LoginRequest, LoginResponse } from './dto/auth.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return { msg: 'It\' s works!'};
+  }
+
+  login(loginRequest: LoginRequest): LoginResponse {
+    if (
+      loginRequest.username === 'usuario1' &&
+      loginRequest.password === '123456'
+    ) {
+      return {
+        name: 'carlos',
+        username: loginRequest.username,
+        lastname: 'gonzalez',
+      };
+    } else {
+      throw new Error('User not found!');
+    }
   }
 }
