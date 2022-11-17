49d216ce040c78719ed121689cfee30bd85356ba
Juan José Rubio Iglesias <jjrubio@talentosoftware.com>
2022-11-17 12:24:01 +0100

    jwt

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { LoginRequest, LoginResponse } from './dto/auth.dto';
-
+import * as moment from 'moment';
 @Injectable()
 export class AppService {
   getHello() {
-    return { msg: 'It\' s works!'};
+    return {msg: 'It\' s works!'};
   }
 
-  login(loginRequest: LoginRequest): LoginResponse {
+  login(loginRequest: LoginRequest) {
     if (
       loginRequest.username === 'usuario1' &&
       loginRequest.password === '123456'
     ) {
-      return {
-        name: 'carlos',
-        username: loginRequest.username,
-        lastname: 'gonzalez',
+      const user = {
+        id: 'userId-1',
+        //rol: loginRequest.username,
+        // rol: 'tenantRead',
+        // rol: 'tenantWrite',
+        // rol: 'reseller',
+        rol: 'agemes',
+        roles: ['agemes', 'tenantRead'],
+        exp: moment().add('2', 'hours').unix(),
       };
+
+      const ret = {
+        access_token: {
+          sub: user.id,
+          roles: user.roles,
+          exp: user.exp,
+          mipayload: 'Mi payload... ',
+        },
+        refresh_token: {
+          sub: user.id,
+          exp: user.exp,
+        },
+        exp: user.exp,
+      };
+      return ret;
     } else {
       throw new Error('User not found!');
     }
